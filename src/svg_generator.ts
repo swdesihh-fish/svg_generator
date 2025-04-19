88ce917913e3cb9ac8c49294a52297ae37045bd8
Daniel <daniel.kang.us@gmail.com>
2025-04-19 15:59:02 -0700

    Fixed collision handling code

--- src/svg_generator.ts
+++ src/svg_generator.ts
@@ -1,14 +1,15 @@
-const fs = require('fs');
-import sky_generator from './sky_generator';
+const fs = require("fs");
+import sky_generator from "./sky_generator";
 
-const svg_generator = () => {
-    const generated_sky = sky_generator(1920, 2560, 200);
-    fs.writeFile('../test.svg', generated_sky, 'utf8', (err: Error) => {
-        if (err) {
-            console.error('Error writing file: ', err);
-            return;
-        }
-    });
-}
+const svg_generator = (width: number, height: number, num_stars: number) => {
+  const generated_sky = sky_generator(width, height, num_stars, true);
+  fs.writeFile("../test.svg", generated_sky, "utf8", (err: Error) => {
+    if (err) {
+      console.error("Error writing file: ", err);
+      return;
+    }
+  });
+  console.log("File written");
+};
 
-export default svg_generator;
+svg_generator(1920, 2560, 300);
